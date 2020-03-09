3988c96cc47b631733e81a2fc37a9f5049954c53
Echo <13135677115@163.com>
2020-03-09 21:48:52 +0800

    v1.0.6

---
+++ my-project-service/my-project-user-service/src/main/java/com/qf/my/project/user/service/MyProjectUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.qf.my.project.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectUserServiceApplication.class, args);
+    }
+
+}
---
+++ my-project-service/my-project-user-service/src/test/java/com/qf/my/project/user/service/MyProjectUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.my.project.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
