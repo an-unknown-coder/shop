ecdbfa3fa63a99ee4e86223e3386970438285fb2
fangwei666 <303044209@qq.com>
2020-03-11 14:39:39 +0800

    3月11方

---
+++ shop-springcloud/my-project-web/product-web/src/main/java/com/qf/controller/test.java
@@ -0,0 +1,18 @@
+package com.qf.controller;
+
+public class test {
+    private String num;
+
+
+    public test(String num) {
+        this.num = num;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+}
