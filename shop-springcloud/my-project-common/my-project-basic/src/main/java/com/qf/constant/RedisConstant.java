7272572abce0339589a33752c2e602d01a8a1f5d
java <baidu.com>
2020-03-14 09:31:47 +0800

    Merge branch 'master' of E:\ke\spring-cloud-shop\shop with conflicts.

--- shop-springcloud/my-project-common/my-project-basic/src/main/java/com/qf/constant/RedisConstant.java
+++ shop-springcloud/my-project-common/my-project-basic/src/main/java/com/qf/constant/RedisConstant.java
@@ -1,8 +1,12 @@
 package com.qf.constant;
 
 public interface RedisConstant {
-    String USER_CART_PRE="user:cart:";
-    String USER_LOGIN_PRE = "user:login:";
-    String PRODUCT_PRE = "product";
+
+    String USER_LOGIN_PRE ="user:login:";
+    String USER_CART_PRE ="user:cart:";
+    String PRODUCT_PRE = "product:";
     String REGISTER_EMAIL = "register:email:";
+
+
+
 }
